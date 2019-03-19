cc0a1372ee12f734c74816ac4936238a98e320c5
legend1b <olumideolayinka2@gmail.com>
2019-03-19 14:10:21 +0100

    Update scriptt.js

--- js/scriptt.js
+++ js/scriptt.js
@@ -4,18 +4,18 @@ var admobid = {};
 // select the right Ad Id according to platform
 if( /(android)/i.test(navigator.userAgent) ) { 
     admobid = { // for Android
-        banner: 'ca-app-pub-5864221037986099/1982240638',
-        interstitial: 'ca-app-pub-5864221037986099/7571987975'
+        banner: 'ca-app-pub-5864221037986099/2809683672',
+        interstitial: 'ca-app-pub-5864221037986099/8556177838'
     };
 } else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
     admobid = { // for iOS
-        banner: 'ca-app-pub-5864221037986099/1982240638',
-        interstitial: 'ca-app-pub-5864221037986099/7571987975'
+        banner: 'ca-app-pub-5864221037986099/2809683672',
+        interstitial: 'ca-app-pub-5864221037986099/8556177838'
     };
 } else {
     admobid = { // for Windows Phone
-        banner: 'ca-app-pub-5864221037986099/1982240638',
-        interstitial: 'ca-app-pub-5864221037986099/7571987975'
+        banner: 'ca-app-pub-5864221037986099/2809683672',
+        interstitial: 'ca-app-pub-5864221037986099/8556177838'
     };
 }
 
